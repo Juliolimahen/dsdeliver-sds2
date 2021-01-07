c97df16f1386a049e33a450ab76e127c1db99cfc
Juliolimahen <juliolima_henrique@outlook.com>
2021-01-07 02:23:54 -0300

    GET /orders

--- backend/src/main/java/com/devsuperior/dsdeliver/dto/ProductDTO.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/dto/ProductDTO.java
@@ -10,6 +10,7 @@ public class ProductDTO  implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
 	private Long id;
 	private String name;
 	private Double price;
