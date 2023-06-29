aff263cdfaba4bccd83b047e18fae939ad034d0e
Juliolimahen <juliolima_henrique@outlook.com>
2023-06-29 19:51:47 -0300

    fix endpoints and units tests

--- front-web/src/Services/api.ts
+++ front-web/src/Services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { OrderPayload, Product } from "../Pages/Orders/types";
 
 const API_URL = process.env.REACT_APP_API_URL ?? 'https://localhost:44369';
-const mapboxToken = process.env.REACT_APP_ACCESS_TOKEN_MAP_BOX ?? 'pk.eyJ1IjoianVsaW8wOTkwOTkiLCJhIjoiY2tqcTZqdTRxMG96bDM0bW5vbTR0YWdzYiJ9.g0UhwSpA0s4KEvqqrh8Kvg';
+const mapboxToken = process.env.REACT_APP_ACCESS_TOKEN_MAP_BOX;
 
 
 export function fetchProducts() {
@@ -14,15 +14,26 @@ export function fetchLocalMapBox(local: string) {
 }
 
 export function saveOrder(payload: OrderPayload) {
-    debugger
-    return axios.post(`${API_URL}/orders`, payload, {
-       headers: {
-            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
-        }
+    return axios.post(`${API_URL}/orders`, JSON.stringify(payload), {
+        headers: {
+            'Content-Type': 'application/json',
+        },
     });
-  }
-  
+}
+
 
 export function saveProduct(product: Product) {
-    return axios.put(`${API_URL}/products/${product.id}`, product);
+    return axios.put(`${API_URL}/products/${product.id}`, JSON.stringify(product), {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+}
+
+export function createProduct(product: Product) {
+    return axios.post(`${API_URL}/products`, product, {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
 }
