636ffff8daa0fce148b95c7986a16bd960752993
Juliolimahen <juliolima_henrique@outlook.com>
2023-08-24 21:42:16 -0300

    Iniciando refatoração

---
+++ back-end-dot-net/DsDeliveryApi/DsDelivery.Data/Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using DsDelivery.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DsDelivery.Data.Repositories.Interfaces
+{
+    public interface IRepository<T> : IDisposable where T : Entity
+    {
+        Task AddAsync(T entity);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int? id);
+        Task UpdateAsync(T entity);
+        Task RemoveAsync(int? id);
+
+        Task<IEnumerable<T>>
+            SearchAsync(Expression<Func<T, bool>> predicate);
+    }
+}
---
+++ back-end-dot-net/DsDeliveryApi/DsDelivery.Data/Repositories/Repository.cs
@@ -0,0 +1,49 @@
+﻿using DsDelivery.Core.Domain;
+using DsDelivery.Data.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DsDelivery.Data.Repositories
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : Entity
+    {
+        public Task AddAsync(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> GetByIdAsync(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveAsync(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<TEntity>> SearchAsync(Expression<Func<TEntity, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
